bbe75adde231a428632f4dc5bd28aaa8f6710b48
Eneibal <nemesistotv1995@gmail.com>
2023-05-26 21:37:40 +0300

    Add observer Class

--- AnotherTry/obj/Debug/net6.0-windows/MainWindow.g.cs
+++ AnotherTry/obj/Debug/net6.0-windows/MainWindow.g.cs
@@ -53,7 +53,7 @@ namespace AnotherTry {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/AnotherTry;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/AnotherTry;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
