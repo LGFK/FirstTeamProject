ec1753d6760d6d531d6cd168b82cac1f5e2e3112
hamlet <107580695+HamletArut@users.noreply.github.com>
2023-05-26 22:22:44 +0300

    New class

--- AnotherTry/obj/Debug/net6.0-windows/App.g.i.cs
+++ AnotherTry/obj/Debug/net6.0-windows/App.g.i.cs
@@ -45,7 +45,7 @@ namespace AnotherTry {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace AnotherTry {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.5.0")]
         public static void Main() {
             AnotherTry.App app = new AnotherTry.App();
             app.InitializeComponent();
