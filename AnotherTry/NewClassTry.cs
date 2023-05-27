3a481fd8f0ddcce7247b31a68c009ddaa4a2bdcd
hamlet <107580695+HamletArut@users.noreply.github.com>
2023-05-27 12:22:32 +0300

    Chanches

---
+++ AnotherTry/NewClassTry.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnotherTry
+{
+    internal class NewClassTry
+    {
+        public string name;
+        public NewClassTry() { }
+    }
+}
