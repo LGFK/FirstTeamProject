2bd2bb0f91c9e9aedaf31436c4f6e56c56e658cf
Eneibal <nemesistotv1995@gmail.com>
2023-05-27 16:55:32 +0300

    Add_WPF

--- AnotherTry/MVVM/ViewModel/DiscoveryViewModel.cs
+++ AnotherTry/MVVM/ViewModel/DiscoveryViewModel.cs
@@ -4,10 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AnotherTry
+namespace AnotherTry.MVVM.ViewModel;
+internal class DiscoveryViewModel
 {
-    internal class IvansClass
-    {
-
-    }
 }
