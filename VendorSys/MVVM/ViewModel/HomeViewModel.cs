2f4a6a1660be7d4c6e67787c5be4ecec9f40f7b3
Eneibal <nemesistotv1995@gmail.com>
2023-05-28 16:32:57 +0300

    Add Start WPF
    Home page
    Catalog

---
+++ VendorSys/MVVM/ViewModel/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VendorSys.Core;
+
+namespace VendorSys.MVVM.ViewModel
+{
+    class HomeViewModel : ObservableObject
+    {
+        public HomeViewModel() { }
+    }
+}
---
+++ VendorSys/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,48 @@
+﻿using VendorSys.Core;
+using VendorSys.MVVM.View;
+
+namespace VendorSys.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand CatalogViewCommand { get; set; }
+        //~~~~~~~~~~//
+        public HomeViewModel HomeVM { get; set; }
+        public CatalogView CatalogVM { get; set; }
+
+
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CatalogVM = new CatalogView();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            CatalogViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = CatalogVM;
+            });
+
+            /*            Page = new RelayCommand(o =>
+                        {
+                            CurrentView = PageVM;
+                        });*/
+
+        }
+    }
+}
