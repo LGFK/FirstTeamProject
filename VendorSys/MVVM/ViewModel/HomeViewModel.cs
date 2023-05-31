32eaa3711ea13cce537af432bb93d7c1d1e9ab75
Eneibal <nemesistotv1995@gmail.com>
2023-05-31 17:48:43 +0300

    Add Search_list
    Add list

--- VendorSys/MVVM/ViewModel/HomeViewModel.cs
+++ VendorSys/MVVM/ViewModel/HomeViewModel.cs
@@ -1,14 +1,75 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VendorSys.Core;
 
-namespace VendorSys.MVVM.ViewModel
+namespace VendorSys.MVVM.ViewModel;
+
+class ProductTest // Приклад 
 {
-    class HomeViewModel : ObservableObject
+    public string? Name { get; set; }
+
+    public string ToLower()
     {
-        public HomeViewModel() { }
+        return Name.ToLower();
+    }
+}
+
+class HomeViewModel : ObservableObject
+{
+    private string _filterText;
+    private List<ProductTest> _data;
+    private List<ProductTest> _filteredData;
+
+    public string FilterText
+    {
+        get { return _filterText; }
+        set
+        {
+            if (_filterText != value)
+            {
+                _filterText = value;
+                FilterData();
+                OnPropertyChanged(nameof(FilterText));
+            }
+        }
+    }
+
+    public List<ProductTest> FilteredData
+    {
+        get { return _filteredData; }
+        set
+        {
+            _filteredData = value;
+            OnPropertyChanged(nameof(FilteredData));
+        }
+    }
+    public HomeViewModel()
+    {
+        /*        _data = new List<string>()
+                {
+                        "Apple", "Banana", "Orange", "Mango", "Grapes", "Watermelon", "Pineapple"
+                };*/
+
+        _data = new List<ProductTest>() { new ProductTest() {Name ="Test1" },
+            new ProductTest() {Name ="Burger" }, 
+            new ProductTest() { Name = "qweqwad" },
+            new ProductTest() { Name = "123edqd" },
+            new ProductTest() {Name ="asdaf" },new ProductTest() {Name ="213dqf" }, 
+            new ProductTest() {Name ="asdf" },new ProductTest() {Name ="Burger" } };
+
+        FilterData();
+    }
+
+    private void FilterData()
+    {
+        if (_filterText != "" && _filterText != null)
+        {
+            _filteredData = _data.Where(item => item.ToLower().Contains(_filterText.ToLower())).ToList();
+        }
+        else
+        {
+            _filteredData = _data;
+        }
+        OnPropertyChanged(nameof(FilteredData));
     }
 }
--- VendorSys/MVVM/ViewModel/MainViewModel.cs
+++ VendorSys/MVVM/ViewModel/MainViewModel.cs
@@ -1,88 +1,91 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.ObjectModel;
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Documents;
+using System.Windows.Data;
 using VendorSys.Core;
 
-namespace VendorSys.MVVM.ViewModel
+namespace VendorSys.MVVM.ViewModel;
+
+class MainViewModel : ObservableObject
 {
-    class MainViewModel : ObservableObject
+    public RelayCommand HomeViewCommand { get; set; }
+    public RelayCommand CatalogViewCommand { get; set; }
+    public RelayCommand BasketViewCommand { get; set; }
+    public RelayCommand ButtonMinimizeClickCommand { get; set; }
+    public RelayCommand WindowsSateButtonClickCommand { get; set; }
+    public RelayCommand CoseButtonClickCommand { get; set; }
+
+    //~~~~~~~~~~//
+    public HomeViewModel HomeVM { get; set; }
+    public CatalogViewModel CatalogVM { get; set; }
+    public BasketViewModel BasketVM { get; set; }
+
+    private object _currentView;
+    public object CurrentView
     {
-        public RelayCommand HomeViewCommand { get; set; }
-        public RelayCommand CatalogViewCommand { get; set; }
-        public RelayCommand BasketViewCommand { get; set; }
-        public RelayCommand ButtonMinimizeClickCommand { get; set; }
-        public RelayCommand WindowsSateButtonClickCommand { get; set; }
-        public RelayCommand CoseButtonClickCommand { get; set; }
-
-        //~~~~~~~~~~//
-        public HomeViewModel HomeVM { get; set; }
-        public CatalogViewModel CatalogVM { get; set; }
-        public BasketViewModel BasketVM { get; set; }
-
-
-
-        private object _currentView;
-        public object CurrentView
+        get { return _currentView; }
+        set
         {
-            get { return _currentView; }
-            set
-            {
-                _currentView = value;
-                OnPropertyChanged();
-            }
-        }
-        public MainViewModel()
-        {
-            SwitchBetweenPages();
-            ButtonLogic();
-
-        }
-
-        private void ButtonLogic()
-        {
-            ButtonMinimizeClickCommand = new RelayCommand(o =>
-            {
-                Application.Current.MainWindow.WindowState = WindowState.Minimized;
-            });
-
-            WindowsSateButtonClickCommand = new RelayCommand(o =>
-            {
-                if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
-                    Application.Current.MainWindow.WindowState = WindowState.Maximized;
-                else
-                    Application.Current.MainWindow.WindowState = WindowState.Normal;
-            });
-
-            CoseButtonClickCommand = new RelayCommand(o =>
-            {
-                Application.Current.Shutdown();
-            });
-        }
-
-        private void SwitchBetweenPages()
-        {
-            HomeVM = new HomeViewModel();
-            CatalogVM = new CatalogViewModel();
-            BasketVM = new BasketViewModel();
-
-            CurrentView = HomeVM;
-
-            HomeViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = HomeVM;
-            });
-
-            CatalogViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = CatalogVM;
-            });
-
-            BasketViewCommand = new RelayCommand(o =>
-            {
-                CurrentView = BasketVM;
-            });
+            _currentView = value;
+            OnPropertyChanged();
         }
     }
+
+
+    public MainViewModel()
+    {
+
+        SwitchBetweenPages();
+        ButtonLogic();
+    }
+
+    private void ButtonLogic()
+    {
+        ButtonMinimizeClickCommand = new RelayCommand(o =>
+        {
+            Application.Current.MainWindow.WindowState = WindowState.Minimized;
+        });
+
+        WindowsSateButtonClickCommand = new RelayCommand(o =>
+        {
+            if (Application.Current.MainWindow.WindowState != WindowState.Maximized)
+                Application.Current.MainWindow.WindowState = WindowState.Maximized;
+            else
+                Application.Current.MainWindow.WindowState = WindowState.Normal;
+        });
+
+        CoseButtonClickCommand = new RelayCommand(o =>
+        {
+            Application.Current.Shutdown();
+        });
+    }
+
+    private void SwitchBetweenPages()
+    {
+        HomeVM = new HomeViewModel();
+        CatalogVM = new CatalogViewModel();
+        BasketVM = new BasketViewModel();
+
+        CurrentView = HomeVM;
+
+        HomeViewCommand = new RelayCommand(o =>
+        {
+            CurrentView = HomeVM;
+        });
+
+        CatalogViewCommand = new RelayCommand(o =>
+        {
+            CurrentView = CatalogVM;
+        });
+
+        BasketViewCommand = new RelayCommand(o =>
+        {
+            CurrentView = BasketVM;
+        });
+    }
+
+
+
+ 
+
 }
