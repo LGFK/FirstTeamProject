db8c5d835544c2f7b05c49a9d5bcb69094c83f67
Eneibal <nemesistotv1995@gmail.com>
2023-06-01 15:53:45 +0300

    WPF_Style
    Add style basket, home,

--- VendorSys/MVVM/Model/ProductRepository.cs
+++ VendorSys/MVVM/Model/ProductRepository.cs
@@ -30,7 +30,16 @@ namespace VendorSys.MVVM.Model
 
             // в колекцію "Products" отримати продукти від сервера.
             Products.Add(new Product(1, "Apple", 200, 5, 1, "image1.jpg", 0, new ProductType(), new List<ProductsSold>()));
-            Products.Add(new Product(2, "Banana", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(3, "Banana", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(4, "Banana1", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(5, "Banana2", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(6, "Banana3", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(7, "Banana4", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(8, "Banana5", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(9, "Banana6", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(10, "Banana7", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(11, "Banana8", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
+            Products.Add(new Product(12, "Banana9", 300, 3, 2, "image2.jpg", 25, new ProductType(), new List<ProductsSold>()));
 
             return Products;
         }
