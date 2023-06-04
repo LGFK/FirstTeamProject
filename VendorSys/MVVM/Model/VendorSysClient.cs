de7c50a5db443039777dcaef7f0a3bb6cc64b88f
AndriiBoyko <105152865+AndriiBoyko@users.noreply.github.com>
2023-06-04 12:44:36 +0300

    Update VendorSysClient.cs

--- VendorSys/MVVM/Model/VendorSysClient.cs
+++ VendorSys/MVVM/Model/VendorSysClient.cs
@@ -237,7 +237,47 @@ namespace VendorSys.MVVM.Model
                 MessageBox.Show(ex.Message);
             }
         }
-        
+        public async Task GetDiscountProductsAsync()
+        {
+            try
+            {
+                var endPoint = new IPEndPoint(IPAddress.Loopback, 1488);
+                TcpClient client = new TcpClient();
+                client.Connect(endPoint);
+
+
+                var networkStream = client.GetStream();
+                buffer = new byte[4];
+                string message = "Products";
+                var requestMessage = Encoding.UTF8.GetBytes(message);
+                buffer = BitConverter.GetBytes(requestMessage.Length);
+                await networkStream.WriteAsync(buffer, 0, buffer.Length);
+                await networkStream.WriteAsync(requestMessage, 0, requestMessage.Length);
+
+                await networkStream.ReadAsync(buffer, 0, buffer.Length);
+                respSize = BitConverter.ToInt32(buffer, 0);
+
+                requestToReceive = new byte[respSize];
+                await networkStream.ReadAsync(requestToReceive, 0, requestToReceive.Length);
+                jsonToReceive = Encoding.UTF8.GetString(requestToReceive);
+                List<Product>? products = new List<Product>();
+                products = JsonConvert.DeserializeObject<List<Product>>(jsonToReceive);
+                client.Close();
+                foreach (var item in products)
+                {
+                    if(item.Discount > 0)
+                    {
+                        Products.Add(new Product(item.Id, item.Pname, item.Price,
+                        item.Amount, item.ProdType, item.Image, item.Discount,
+                        item.ProdTypeNavigation, item.ProductsSolds));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
 
     }
 }
