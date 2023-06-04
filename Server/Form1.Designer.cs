281180f80e78dcc20a1c84dbc4ed53d1e45d9738
E-one <potlod13@gmail.com>
2023-06-04 18:44:07 +0300

    Added couple of methods to the server

--- Server/Form1.Designer.cs
+++ Server/Form1.Designer.cs
@@ -28,15 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.serverStart = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // serverStart
             // 
+            this.serverStart.AutoSize = true;
             this.serverStart.BackColor = System.Drawing.Color.IndianRed;
-            this.serverStart.Location = new System.Drawing.Point(193, 192);
+            this.serverStart.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.serverStart.Location = new System.Drawing.Point(0, 0);
             this.serverStart.Name = "serverStart";
-            this.serverStart.Size = new System.Drawing.Size(108, 55);
+            this.serverStart.Size = new System.Drawing.Size(525, 55);
             this.serverStart.TabIndex = 3;
             this.serverStart.Text = "Start Server";
             this.serverStart.UseVisualStyleBackColor = false;
@@ -46,12 +49,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(525, 450);
+            this.ClientSize = new System.Drawing.Size(525, 55);
             this.Controls.Add(this.serverStart);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Server";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
