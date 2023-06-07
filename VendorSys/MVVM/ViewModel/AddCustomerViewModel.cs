36e677fc833b3e6961ff83e8e8e4ec5b3933a8f2
Eneibal <nemesistotv1995@gmail.com>
2023-06-07 21:27:56 +0300

    Add FIx

--- VendorSys/MVVM/ViewModel/AddCustomerViewModel.cs
+++ VendorSys/MVVM/ViewModel/AddCustomerViewModel.cs
@@ -3,9 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using VendorSys.Core;
 
 namespace VendorSys.MVVM.ViewModel;
-internal class AddCustomerViewModel
+internal class AddCustomerViewModel : ObservableObject
 {
-    public AddCustomerViewModel() { }
+    private string? _firstName;
+    private string? _lastName;
+    private string? _email;
+    private string? _phone;
+    public RelayCommand AddCommand { get; set; }
+    public string FirstName
+    {
+        get => _firstName;
+        set
+        {
+            if (_firstName != value)
+            {
+                _firstName = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+    public string? LastName { get => _lastName; set => _lastName = value; }
+    public string? Email { get => _email; set => _email = value; }
+    public string? Phone { get => _phone; set => _phone = value; }
+    public AddCustomerViewModel()
+    {
+        AddCommand = new RelayCommand(o =>
+        {
+            MessageBox.Show($"{FirstName}\n{LastName}\n{Email}\n{Phone}");
+        });
+    }
 }
